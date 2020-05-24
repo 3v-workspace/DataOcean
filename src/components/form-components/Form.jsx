1c0b35e72374eed7059be7450951517377394831
Roman Tiukh <frogour@gmail.com>
2020-05-24 21:14:28 +0300

    add Form component with support formik

---
+++ src/components/form-components/Form.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Form = (props) => {
+  const {
+    children, noValidate, className, formik, onSubmit,
+  } = props;
+
+  const classList = [];
+  if (className) {
+    classList.push(className);
+  }
+  if (!noValidate) {
+    classList.push('validate-form');
+  }
+
+  return (
+    <form
+      className={classList.join(' ')}
+      onSubmit={onSubmit || formik.handleSubmit}
+      onReset={formik.handleReset}
+    >
+      {children}
+    </form>
+  );
+};
+
+Form.propTypes = {
+  onSubmit: PropTypes.func,
+  className: PropTypes.string,
+  formik: PropTypes.shape({
+    handleSubmit: PropTypes.func.isRequired,
+    handleReset: PropTypes.func.isRequired,
+  }),
+  noValidate: PropTypes.bool,
+};
+Form.defaultProps = {
+  onSubmit: null,
+  className: '',
+  noValidate: false,
+  formik: null,
+};
+
+export default Form;
