d6fa67e1b49691052618dd2b82d4b36cafc53785
Roman Tiukh <frogour@gmail.com>
2020-05-25 20:31:39 +0300

    create state of RestorePassword forms

--- src/components/pages/auth/RestorePassword.jsx
+++ src/components/pages/auth/RestorePassword.jsx
@@ -3,6 +3,9 @@ import TextInput from 'components/form-components/TextInput';
 import Button from 'components/form-components/Button';
 import PropTypes from 'prop-types';
 import { Redirect } from 'react-router-dom';
+import Form from 'components/form-components/Form';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 
 const stages = {
   START: 'START',
@@ -10,82 +13,143 @@ const stages = {
   RESTORE: 'RESTORE',
 };
 
-const EmailForm = ({ setStage }) => (
-  <>
-    <div className="intro-x mt-8">
-      <TextInput
-        className="intro-x login__input border-gray-300 block"
-        name="email"
-        type="email"
-        placeholder="Email"
-      />
-    </div>
-    <div className="intro-x mt-5 xl:mt-8 text-center xl:text-left">
-      <Button
-        className="xl:w-1/2 xl:mr-3"
-        size="lg"
-        width="w-full"
-        onClick={() => setStage(stages.CODE)}
-      >
-        Відновити пароль
-      </Button>
-    </div>
-  </>
-);
+const EmailForm = ({ setStage }) => {
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+    },
+    validationSchema: Yup.object({
+      email: Yup.string()
+        .required('Поле не може бути пусте.')
+        .email('Не коректний Email.'),
+    }),
+    onSubmit: () => {
+      // TODO:
+      setStage(stages.RESTORE);
+    },
+  });
+
+  return (
+    <Form formik={formik}>
+      <div className="intro-x mt-8">
+        <TextInput
+          className="intro-x login__input border-gray-300 block"
+          name="email"
+          type="email"
+          placeholder="Email"
+          formik={formik}
+        />
+      </div>
+      <div className="intro-x mt-5 xl:mt-8 text-center xl:text-left">
+        <Button
+          type="submit"
+          className="xl:w-1/2 xl:mr-3"
+          size="lg"
+          width="w-full"
+        >
+          Відновити пароль
+        </Button>
+      </div>
+    </Form>
+  );
+};
 EmailForm.propTypes = { setStage: PropTypes.func.isRequired };
 
-const CodeForm = ({ setStage }) => (
-  <>
-    <div className="intro-x mt-8">
-      <TextInput
-        className="intro-x login__input border-gray-300 block"
-        name="code"
-        type="text"
-        placeholder="Код з email листа"
-      />
-    </div>
-    <div className="intro-x mt-5 xl:mt-8 text-center xl:text-left">
-      <Button
-        className="xl:w-1/2 xl:mr-3"
-        size="lg"
-        width="w-full"
-        onClick={() => setStage(stages.RESTORE)}
-      >
-        Далі
-      </Button>
-    </div>
-  </>
-);
+const CodeForm = ({ setStage }) => {
+  const formik = useFormik({
+    initialValues: {
+      code: '',
+    },
+    validationSchema: Yup.object({
+      code: Yup.string().length(4, 'Поле має містити 4 символи.'),
+    }),
+    onSubmit: () => {
+      // TODO:
+      setStage(stages.RESTORE);
+    },
+  });
+
+  return (
+    <Form formik={formik}>
+      <div className="intro-x mt-8">
+        <TextInput
+          className="intro-x login__input border-gray-300 block"
+          name="code"
+          type="text"
+          placeholder="Код з email листа"
+          formik={formik}
+        />
+      </div>
+      <div className="intro-x mt-5 xl:mt-8 text-center xl:text-left">
+        <Button
+          type="submit"
+          className="xl:w-1/2 xl:mr-3"
+          size="lg"
+          width="w-full"
+        >
+          Далі
+        </Button>
+      </div>
+    </Form>
+  );
+};
 CodeForm.propTypes = { setStage: PropTypes.func.isRequired };
 
-const RestoreForm = ({ setStage }) => (
-  <>
-    <div className="intro-x mt-8">
-      <TextInput
-        className="intro-x login__input border-gray-300 block"
-        name="password1"
-        type="password"
-        placeholder="Пароль 1"
-      />
-      <TextInput
-        className="intro-x login__input border-gray-300 block"
-        name="password2"
-        type="password"
-        placeholder="Пароль 2"
-      />
-    </div>
-    <div className="intro-x mt-5 xl:mt-8 text-center xl:text-left">
-      <Button
-        className="xl:w-1/2 xl:mr-3"
-        size="lg"
-        width="w-full"
-        onClick={() => setStage(stages.RESTORE)}
-      >
-        Далі
-      </Button>
-    </div>
-  </>
-);
+const RestoreForm = ({ setStage }) => {
+  const formik = useFormik({
+    initialValues: {
+      password: '',
+      password2: '',
+    },
+    validate: (values) => {
+      const errors = {};
+      const { password, password2 } = values;
+      if (password !== password2) {
+        errors.password2 = 'Паролі не співпадають.';
+      }
+      return errors;
+    },
+    validationSchema: Yup.object({
+      password: Yup.string().required('Поле не може бути пусте.'),
+      password2: Yup.string().required('Поле не може бути пусте.'),
+    }),
+    onSubmit: () => {
+      // TODO:
+      setStage(stages.CODE);
+    },
+  });
+
+  return (
+    <Form formik={formik}>
+      <div className="intro-x mt-8">
+        <TextInput
+          className="intro-x login__input border-gray-300 block"
+          type="password"
+          name="password"
+          placeholder="Пароль"
+          formik={formik}
+        />
+        <TextInput
+          className="intro-x login__input border-gray-300 block"
+          type="password"
+          name="password2"
+          placeholder="Підтвердження паролю"
+          formik={formik}
+        />
+      </div>
+      <div className="intro-x mt-5 xl:mt-8 text-center xl:text-left">
+        <Button
+          type="submit"
+          className="xl:w-1/2 xl:mr-3"
+          size="lg"
+          width="w-full"
+        >
+          Далі
+        </Button>
+      </div>
+    </Form>
+  );
+};
 RestoreForm.propTypes = { setStage: PropTypes.func.isRequired };
 
 const forms = {
@@ -97,8 +161,8 @@ const forms = {
 const RestorePassword = () => {
   const [stage, setStage] = useState(stages.START);
 
-  const Form = forms[stage];
-  if (!Form) {
+  const FormComponent = forms[stage];
+  if (!FormComponent) {
     return <Redirect to="/404/" />;
   }
 
@@ -107,7 +171,7 @@ const RestorePassword = () => {
       <h2 className="intro-x font-bold text-2xl xl:text-3xl text-center xl:text-left">
         Відновлення паролю
       </h2>
-      <Form
+      <FormComponent
         setStage={setStage}
       />
     </>
