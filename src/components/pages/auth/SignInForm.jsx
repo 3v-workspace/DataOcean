8ba72115dd8d4413527f1adf771d816e6ac49605
Roman Tiukh <frogour@gmail.com>
2020-05-25 22:21:06 +0300

    improve SignInForm

--- src/components/pages/auth/SignInForm.jsx
+++ src/components/pages/auth/SignInForm.jsx
@@ -17,12 +17,16 @@ const SignInForm = () => {
       remember_me: false,
     },
     validationSchema: Yup.object({
-      email: Yup.string().email().required(),
-      password: Yup.string().required(),
+      email: Yup.string()
+        .required('Поле не може бути пусте')
+        .email('Некоректний Email'),
+      password: Yup.string().required('Поле не може бути пусте'),
       remember_me: Yup.bool(),
     }),
     onSubmit: (values, actions) => {
       setTimeout(() => {
+        // TODO: ajax login request
+        actions.setFieldError('password', 'Невірний логін або пароль');
         actions.setSubmitting(false);
       }, 2000);
     },
@@ -51,7 +55,7 @@ const SignInForm = () => {
           name="password"
           className="intro-x login__input border-gray-300 block mt-4"
           size="lg"
-          placeholder="Password"
+          placeholder="Пароль"
           formik={formik}
         />
       </div>
@@ -62,13 +66,14 @@ const SignInForm = () => {
           label="Remember me"
           formik={formik}
         />
-        <Link to="/auth/restore-pass/">Forgot Password?</Link>
+        <Link to="/auth/restore-pass/">Забули пароль?</Link>
       </div>
       <div className="intro-x mt-5 xl:mt-8 text-center xl:text-left">
         <Button
           type="submit"
+          disabled={formik.isSubmitting}
+          isLoading={formik.isSubmitting}
           className="xl:w-32 xl:mr-3"
-          size="lg"
           width="w-full"
         >
           Вхід
@@ -76,7 +81,6 @@ const SignInForm = () => {
         <Button
           className="xl:w-32 xl:mr-3"
           variant="secondary"
-          size="lg"
           width="w-full"
           link="/auth/sign-up/"
         >
