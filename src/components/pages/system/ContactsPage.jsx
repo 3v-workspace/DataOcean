0a435d77144654c7f11812211088f359493c8ada
AlenaYanish <yanishevskayaar@gmail.com>
2020-10-13 09:19:37 +0300

    content on contact page

--- src/components/pages/system/ContactsPage.jsx
+++ src/components/pages/system/ContactsPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import Alert from 'components/Alert';
+//import Alert from 'components/Alert';
 
 const ContactsPage = () => {
   const { t } = useTranslation();
@@ -9,13 +9,24 @@ const ContactsPage = () => {
       <h2 className="intro-y text-lg font-medium mt-10">
         {t('contacts')}
       </h2>
-      <Alert variant="primary" className="intro-y mt-10">
-        {t('inDevelopment')}...
-      </Alert>
+      <div className="intro-y mt-10">
+        Наша електронна адреса:
+      </div>
+      <a href="mailto:info@dataocean.us">info@dataocean.us</a>
+      <div className="intro-y mt-10">
+        Слідкуйте за нашими новинами:
+        <a href="https://www.linkedin.com/company/data-ocean/" target="_blank">
+          <img src="/images/linkedin.png" alt="Linked in" />
+        </a>
+        <a href="https://www.facebook.com/DataOceanGroup/" target="_blank">
+          <img src="/images/facebook.png" alt="Facebook" />
+        </a>
+      </div>
+      <img src="https://dev.virtualearth.net/REST/v1/Imagery/Map/CanvasLight/Kyiv,%20UA?key=Atn679uHwJz5qBW7Qdydz8-WNiejypvCQM1WRC94l9MtNSsBeNwHcLXzUAfFPaBr&amp;mapSize=552,350" alt="Map of  Kyiv,   UA" />
     </>
   );
 };
 
-// ContactsPage.propTypes = {};
+/* ContactsPage.propTypes = {};  */
 
 export default ContactsPage;
