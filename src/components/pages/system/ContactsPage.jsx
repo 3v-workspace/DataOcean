ac7e297aa31f8a03a51fa91e38cce34e756ab89e
AlenaYanish <yanishevskayaar@gmail.com>
2020-10-13 14:48:30 +0300

    content on help page

--- src/components/pages/system/ContactsPage.jsx
+++ src/components/pages/system/ContactsPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-//import Alert from 'components/Alert';
+import Alert from 'components/Alert';
 
 const ContactsPage = () => {
   const { t } = useTranslation();
@@ -10,19 +10,23 @@ const ContactsPage = () => {
         {t('contacts')}
       </h2>
       <div className="intro-y mt-10">
-        Наша електронна адреса:
+        Напишіть нам:{t(' ')}
+        <a href="mailto:info@dataocean.us">info@dataocean.us{t(' ')}</a>
+        <img className="inline" src="/images/mail.png" alt="Mail" />
       </div>
-      <a href="mailto:info@dataocean.us">info@dataocean.us</a>
       <div className="intro-y mt-10">
-        Слідкуйте за нашими новинами:
+        Слідкуйте за нашими новинами:{t(' ')}
         <a href="https://www.linkedin.com/company/data-ocean/" target="_blank">
-          <img src="/images/linkedin.png" alt="Linked in" />
+          <img className="inline-flex" src="/images/linkedin.png" alt="Linked in" />
         </a>
+        {t(' ')}
         <a href="https://www.facebook.com/DataOceanGroup/" target="_blank">
-          <img src="/images/facebook.png" alt="Facebook" />
+          <img className="inline-flex" src="/images/facebook.png" alt="Facebook" />
         </a>
       </div>
-      <img src="https://dev.virtualearth.net/REST/v1/Imagery/Map/CanvasLight/Kyiv,%20UA?key=Atn679uHwJz5qBW7Qdydz8-WNiejypvCQM1WRC94l9MtNSsBeNwHcLXzUAfFPaBr&amp;mapSize=552,350" alt="Map of  Kyiv,   UA" />
+      <div>
+        <img src="https://dev.virtualearth.net/REST/v1/Imagery/Map/CanvasLight/Kyiv,%20UA?key=Atn679uHwJz5qBW7Qdydz8-WNiejypvCQM1WRC94l9MtNSsBeNwHcLXzUAfFPaBr&amp;mapSize=552,350" alt="Map of  Kyiv,   UA" />
+      </div>
     </>
   );
 };
