5ea31913c38f79a1d00b08c207885e023a33193f
AlenaYanish <yanishevskayaar@gmail.com>
2020-11-26 16:44:16 +0200

    contact form

--- src/components/pages/system/ContactsPage.jsx
+++ src/components/pages/system/ContactsPage.jsx
@@ -2,10 +2,33 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { Button } from 'components/form-components';
 import { Facebook, Linkedin, Mail } from 'react-feather';
-//import Alert from 'components/Alert';
+import { useFormik } from 'formik';
+import Form from 'components/form-components/Form';
+import TextInput from 'components/form-components/TextInput';
+import Api from 'api';
+import Yup from 'utils/yup';
 
 const ContactsPage = () => {
   const { t } = useTranslation();
+
+  const formik = useFormik({
+    initialValues: {
+      text: '',
+    },
+    validationSchema: Yup.object({
+      text: Yup.string().required().min(5).max(500),
+    }),
+    onSubmit: (values, actions) => {
+      Api.post('users/question/create/', values)
+        .then(() => {
+          actions.resetForm();
+          $.toast(t('userQuestion'));
+        })
+        .finally(() => {
+          actions.setSubmitting(false);
+        });
+    },
+  });
 
   return (
     <>
@@ -14,13 +37,37 @@ const ContactsPage = () => {
       </h2>
       <div className="intro-y box inline-grid grid-cols-2">
         <div className="inline-grid">
-          <div className="mx-10 pt-10">
-            <span className="font-medium mx-10">{t('contactWrite')}:</span>
-            <a className="items-center flex justify-center mt-5" href="mailto:info@dataocean.us"><Mail className="w-4 h-4 mr-2" />info@dataocean.us</a>
+          <div className="mx-10 pt-6">
+            <h2 className="font-medium pb-4">{t('anyQuestion')} {t('contactWrite')}!</h2>
+            <Form formik={formik}>
+              <TextInput
+                textarea
+                name="text"
+                type="text"
+                className="h-40 intro-x border-gray-200 block focus:outline-none justify-items-start"
+                placeholder={t('placeholderQuestion')}
+                size="lg"
+                formik={formik}
+              />
+              <Button
+                type="submit"
+                className="px-8 mt-4"
+                disabled={formik.isSubmitting}
+              >
+                {t('send')}
+              </Button>
+            </Form>
           </div>
-          <div className="mx-10 pt-5 border-t border-gray-200">
-            <span className="mx-10 my-20 pt-10 font-medium">{t('followOurNews')}:</span>
-            <div className="flex items-center justify-center mt-5">
+          <div className="mx-10 pt-6">
+            <span className="font-medium">{t('emailAddress')}:
+              <a className="items-center flex mt-2 text-blue-800" href="mailto:info@dataocean.us">
+                <Mail className="w-4 h-4 mr-2" />info@dataocean.us
+              </a>
+            </span>
+          </div>
+          <div className="mx-10 pt-6 border-gray-200">
+            <span className="my-20 pt-10 font-medium">{t('followOurNews')}:</span>
+            <div className="flex items-center mt-2">
               <Button className="w-32 mr-2 mb-2 bg-theme-35 cursor-pointer" href="https://www.linkedin.com/company/data-ocean/" target="_blank"><Linkedin className="w-4 h-4 mr-2" />Linkedin</Button>
               <Button className="w-32 mr-2 mb-2 bg-theme-32 cursor-pointer" href="https://www.facebook.com/DataOceanGroup/" target="_blank"><Facebook className="w-4 h-4 mr-2" />Facebook</Button>
             </div>
