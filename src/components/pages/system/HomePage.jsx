cf106a9d265e8559e7a531a77c4b517788f13e9a
Litsyshyn <71875485+Litsyshyn@users.noreply.github.com>
2021-01-13 11:29:05 +0200

    Changed elements ID format
    ID format changed to suggested

--- src/components/pages/system/HomePage.jsx
+++ src/components/pages/system/HomePage.jsx
@@ -90,8 +90,8 @@ const HomePage = ({ history }) => {
     const labels = topKvedData.map((el) => el.kved.code);
     const data = topKvedData.map((el) => el.count_companies_with_kved);
 
-    if ($('#TopKvedChart').length) {
-      const ctx = $('#TopKvedChart')[0].getContext('2d');
+    if ($('#top-kved-chart').length) {
+      const ctx = $('#top-kved-chart')[0].getContext('2d');
       new Chart(ctx, {
         type: 'pie',
         data: {
@@ -120,8 +120,8 @@ const HomePage = ({ history }) => {
     const labels = topCompanyTypeData.map((el) => getName(el));
     const data = topCompanyTypeData.map((el) => el.count_companies);
 
-    if ($('#TopCompanyChart').length) {
-      const ctx = $('#TopCompanyChart')[0].getContext('2d');
+    if ($('#top-company-chart').length) {
+      const ctx = $('#top-company-chart')[0].getContext('2d');
       new Chart(ctx, {
         type: 'pie',
         data: {
@@ -304,7 +304,7 @@ const HomePage = ({ history }) => {
               {/*<a href="#" className="ml-auto text-theme-1 truncate">{t('all')}</a>*/}
             </div>
             <div className="intro-y box p-5 mt-5">
-              <canvas className="mt-3" id="TopKvedChart" height="280" />
+              <canvas className="mt-3" id="top-kved-chart" height="280" />
               <PieChartLegend
                 items={topKvedData.map((el) => ({
                   label: el.kved.code,
@@ -321,7 +321,7 @@ const HomePage = ({ history }) => {
               {/*<a href="#" className="ml-auto text-theme-1 truncate">{t('all')}</a>*/}
             </div>
             <div className="intro-y box p-5 mt-5">
-              <canvas className="mt-3" id="TopCompanyChart" height="280" />
+              <canvas className="mt-3" id="top-company-chart" height="280" />
               <PieChartLegend
                 items={topCompanyTypeData.map((el) => ({
                   label: getName(el),
