3f58a434dbcf4cf8034aae6a8afc0a6d935cd783
Litsyshyn <71875485+Litsyshyn@users.noreply.github.com>
2021-01-22 16:16:48 +0200

    DO-955 update. REACT_APP_LANDING_URL
    link type changed from direct to REACT_APP_LANDING_URL link

--- src/components/pages/auth/SignInForm.jsx
+++ src/components/pages/auth/SignInForm.jsx
@@ -130,7 +130,7 @@ const SignInForm = () => {
           {t('privacyPolicy')}
           {/* {i18n.language === 'en' ? 'Українська' : 'English'} */}
         </a>
-        <p>{t('copyrights')}</p>
+        <p>©2020 – Data Ocean. {t('allRightsReserved')}</p>
       </div>
     </Form>
   );
