bdf21c01afbcd19e6a2f039228c22e3f2dddc648
Andrii17s <75915283+Andrii17s@users.noreply.github.com>
2021-02-17 21:54:18 +0200

    [DO-1277] Implement Design of Button
    Fixing Name Surname without ""

--- src/components/pages/payment/ProjectDetail.jsx
+++ src/components/pages/payment/ProjectDetail.jsx
@@ -309,7 +309,7 @@ const ProjectDetail = (props) => {
         <DeleteModal
           ref={deleteUserModalRef}
           header={`${t('deleteUser')}?`}
-          message={t('payment_system.deleteUserModalMessage', { user: selectedUser.name || '' })}
+          message={t('payment_system.deleteUserModalMessage', { name: selectedUser.name || '' })}
           onDelete={deleteUser}
         />
         <YesNoModal
