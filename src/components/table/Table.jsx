8dd991ba24b768ffc62a89b17568a80a1d255617
Keeslow <aptsiaurioleg@gmail.com>
2021-03-22 17:45:56 +0200

    tablefix

--- src/components/table/Table.jsx
+++ src/components/table/Table.jsx
@@ -106,7 +106,7 @@ const Table = (props) => {
               {columns.map((col) => (
                 <th
                   key={col.prop}
-                  className="border-b-2 whitespace-no-wrap cursor-pointer"
+                  className="border-b-2 whitespace-no-wrap cursor-pointer w-1/5"
                 >
                   <div className="flex items-center justify-between" onClick={() => handleHeaderClick(col)}>
                     {col.header}
