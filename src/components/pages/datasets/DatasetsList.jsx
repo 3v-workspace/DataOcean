cc855462dbea1ea4b73b3f11a9cd37d1ea29e0f2
Keeslow <aptsiaurioleg@gmail.com>
2021-04-05 13:24:57 +0300

    searchplusdataset

--- src/components/pages/datasets/DatasetsList.jsx
+++ src/components/pages/datasets/DatasetsList.jsx
@@ -81,7 +81,7 @@ const DatasetsList = ({ match, history }) => {
                     }
                   }}
                   key={item.id}
-                  className={item.api_list in datasets ? 'intro-x cursor-pointer hover:shadow-xl' : 'cursor-not-allowed'}
+                  className={`'intro-x' ${item.api_list in datasets ? 'cursor-pointer hover:shadow-xl' : 'cursor-not-allowed'}`}
                 >
                   <td>
                     {item.id}
