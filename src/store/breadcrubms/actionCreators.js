63e519231ea82c9fc49ab57341b8410d48fd2eca
Keeslow <aptsiaurioleg@gmail.com>
2021-04-28 17:10:13 +0300

    breadsfin

--- src/store/breadcrubms/actionCreators.js
+++ src/store/breadcrubms/actionCreators.js
@@ -1,9 +1,11 @@
+import { SET_BREADCRUBMS, CHANGE_CRUMB_NAME } from './actions';
+
 export const setBreadcrumbs = (breadcrumbs) => ({
-  type: 'SET_BREADCRUBMS',
+  type: SET_BREADCRUBMS,
   payload: breadcrumbs,
 });
 
 export const changeCrumbName = (index, name) => ({
-  type: 'CHANGE_CRUMB_NAME',
+  type: CHANGE_CRUMB_NAME,
   payload: { index, name },
 });
--- src/store/breadcrubms/breadrumbsReducer.js
+++ src/store/breadcrubms/breadrumbsReducer.js
@@ -1,17 +1,12 @@
+import { SET_BREADCRUBMS, CHANGE_CRUMB_NAME } from './actions';
 
-const initialState = [
-  // {
-  //  name: '',
-  //  link: '',
-  // },
-];
 
-const breadcrumbsReducer = (state = initialState, action) => {
+const breadcrumbsReducer = (state = [], action) => {
   switch (action.type) {
-    case 'SET_BREADCRUBMS':
+    case SET_BREADCRUBMS:
       return action.payload;
 
-    case 'CHANGE_CRUMB_NAME':
+    case CHANGE_CRUMB_NAME:
       state[action.payload.index].name = action.payload.name;
       return [...state];
 
