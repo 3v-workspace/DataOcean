076b8f19a034a9b9ef3b97545a4e5bf5a557d086
Iva-khar <ivakhar@gmail.com>
2021-05-21 12:50:05 +0300

    add formik & validation

--- src/components/pages/auth/SignUpForm.jsx
+++ src/components/pages/auth/SignUpForm.jsx
@@ -49,6 +49,7 @@ const SignUpForm = () => {
     initialValues: {
       first_name: '',
       last_name: '',
+      organization: '',
       email: '',
       password1: '',
       password2: '',
@@ -78,6 +79,7 @@ const SignUpForm = () => {
     validationSchema: Yup.object({
       first_name: Yup.string().name().required().min(2),
       last_name: Yup.string().name().required().min(2),
+      organization: Yup.string().required().min(2),
       email: Yup.string().required().email(),
       phone: Yup.string().phone(),
       password1: Yup.string().required().min(8),
