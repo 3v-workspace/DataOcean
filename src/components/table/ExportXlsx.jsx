2dbc8b76f634be7257cb038ef35a846ed1de2f26
Iva-khar <ivakhar@gmail.com>
2021-07-16 21:12:42 +0300

    export pep button

--- src/components/table/ExportXlsx.jsx
+++ src/components/table/ExportXlsx.jsx
@@ -13,35 +13,44 @@ const ExportXlsx = (props) => {
   const { t } = useTranslation();
   const { exportUrl, params, count } = props;
   const ExportXlsxModalRef = useRef();
-  const [ResponceMessage, setResponceMessage] = useState([]);
   const detail = 'detail';
-
-  const exportXlsx = () => {
-    Api.get(`${exportUrl}?${params}`)
-      .then((resp) => {
-        setResponceMessage(resp.data[detail]);
-      });
-    toast('success', ResponceMessage);
-    ExportXlsxModalRef.current.hide();
-  };
+  let element;
 
   const openExportXlsxModal = () => {
     ExportXlsxModalRef.current.show();
   };
 
-  let element;
+  const exportXlsx = () => {
+    Api.get(`${exportUrl}?${params}`)
+      .then((resp) => {
+        toast('success', resp.data[detail]);
+      });
+    ExportXlsxModalRef.current.hide();
+  };
+
   if (count <= EXPORT_PEP_XLSX_LIMIT) {
     element = (
       <div onClick={openExportXlsxModal} className="flex mx-2 cursor-pointer text-gray-800">
         <Download className="w-5 h-5 mr-1 color-gray-800" />
         Excel
-        <YesNoModal ref={ExportXlsxModalRef} onYes={exportXlsx} />
+        <YesNoModal
+          ref={ExportXlsxModalRef}
+          onYes={exportXlsx}
+          header={t('downloadXlsx')}
+          message={t('downloadXlsxConfirmation')}
+        />
       </div>
     );
   } else {
     element = (
-      <Tooltip position="bottom" arrow={false} content={t('exportTooltipContent', { limit: EXPORT_PEP_XLSX_LIMIT })} className="flex mx-2 cursor-default text-gray-500">
-        <Download className="w-5 h-5 mr-1 color-gray-500" /> Excel
+      <Tooltip
+        position="bottom"
+        arrow={false}
+        content={t('exportTooltipContent', { limit: EXPORT_PEP_XLSX_LIMIT })}
+        className="flex mx-2 cursor-default text-gray-500"
+      >
+        <Download className="w-5 h-5 mr-1 color-gray-500" />
+        Excel
       </Tooltip>
     );
   }
