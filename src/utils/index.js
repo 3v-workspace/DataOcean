ae0a8a6f04ffbaac8ad3ac8eae2dfff2c240ba68
Andrii17s <75915283+Andrii17s@users.noreply.github.com>
2021-07-27 15:47:09 +0300

    Revert "itter 1"
    This reverts commit f11a1852244e1e8c67911b04fd2981a3c523a76f.

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,4 @@
 import moment from 'moment';
-import { DATE_FORMAT, DATETIME_FORMAT, DATE_FORMAT_ENG, DATETIME_FORMAT_ENG } from 'const/const';
-import { useTranslation } from 'react-i18next';
 
 export { default as toggleFullScreen } from './fullscreen';
 export { default as setLanguage } from './setLanguage';
@@ -8,13 +6,9 @@ export { default as toast } from './toast';
 
 
 export const upFirstLetter = (string) => string.charAt(0).toUpperCase() + string.slice(1);
-const Format = (string) => {
-  const { i18n } = useTranslation();
-  return (i18n.language !== 'en') ? DATE_FORMAT_ENG : DATE_FORMAT;
-};
 
-export const dateFormat = (iso) => moment(iso).format(Format());
+export const dateFormat = (iso) => moment(iso).format('MMMM D, YYYY');
 
-export const dateFormatISO = (iso) => moment(iso).format(DATE_FORMAT_ENG);
+export const dateFormatISO = (iso) => moment(iso).format('YYYY-MM-DD');
 
-export const datetimeFormat = (iso) => moment(iso).format(DATETIME_FORMAT_ENG);
+export const datetimeFormat = (iso) => moment(iso).format('MMMM D, YYYY HH:mm');
