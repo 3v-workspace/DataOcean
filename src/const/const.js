08d4a5f8849149a16b94606cee98e5a94b427960
Andrii17s <75915283+Andrii17s@users.noreply.github.com>
2021-07-27 17:30:27 +0300

    +eng constants

--- src/const/const.js
+++ src/const/const.js
@@ -3,6 +3,8 @@
 export const DATE_FORMAT = 'DD MMMM YYYY';
 export const TIME_FORMAT = 'HH:mm:ss';
 export const DATETIME_FORMAT = 'DD MMMM YYYY HH:mm:ss';
+export const DATE_FORMAT_ENG = 'MMM DD, YYYY';
+export const DATETIME_FORMAT_ENG = 'MMM DD, YYYY HH:mm:ss';
 
 export const PROJECT_TOKEN_PREFIX = 'Platform';
 
