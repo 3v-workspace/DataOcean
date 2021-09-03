78ec39e51d742230573f05145209d625c0138a45
AlenaYanish <yanishevskayaar@gmail.com>
2021-09-03 12:46:34 +0300

    [DO-2377&&DO-2378&&DO-2376]Make changes to the look of the PERSONAL page of the sanctions register against country,company,person

--- src/components/pages/datasets/sanctions/PersonSanctionDetail.jsx
+++ src/components/pages/datasets/sanctions/PersonSanctionDetail.jsx
@@ -1,17 +1,20 @@
 import React, { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { renderDate } from 'utils';
-import { useParams } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { renderDate, upFirstLetter } from 'utils';
+import { useParams, useHistory } from 'react-router-dom';
 import Api from 'api';
-import UnfoldingBlock from 'components/UnfoldingBlock';
-import { Download } from 'react-feather';
+import { Download, Printer, ArrowLeft } from 'react-feather';
 import Tooltip from 'components/Tooltip';
-
+import { ReactComponent as ImgPerson } from 'images/logo_person.svg';
+import { setTopBarShow } from 'store/interface/actionCreators';
 
 const PersonSanctionDetail = () => {
   const [data, setData] = useState({});
   const { id } = useParams();
   const { t, i18n } = useTranslation();
+  const dispatch = useDispatch();
+  const history = useHistory();
 
   const fetchData = () => {
     Api.get(`sanction/person/${id}/`, { useProjectToken: true })
@@ -22,23 +25,80 @@ const PersonSanctionDetail = () => {
 
   const getSanctions = (sanctions) => {
     if (!sanctions.length) {
-      return '---';
+      return null;
     }
     return (
-      <ul className="list-disc list-inside">
-        {sanctions.map((sanction) => (
-          <li key={sanction}>
-            <span>
-              {sanction}
-            </span>
-          </li>
-        ))}
-      </ul>
+      <div className="intro-y mt-6 col-span-12">
+        <div className="overflow-auto md:overflow-hidden">
+          <table className="table">
+            <thead className="rounded-md">
+              <tr className="bg-gray-200 text-gray-800 font-medium rounded-md">
+                <th>{t('typeOfSanction')}</th>
+                <th>{t('startDate')}</th>
+                <th>{t('endDate')}</th>
+                <th>{t('reasoningDate')}</th>
+                <th>{t('cancellationCondition')}</th>
+              </tr>
+            </thead>
+            <tbody>
+              {sanctions.map((sanction) => (
+                <tr
+                  key={sanction}
+                  className="text-gray-700 border-b border-gray-200"
+                >
+                  <td>{sanction}</td>
+                  <td>{renderDate(data.start_date)}</td>
+                  <td>{renderDate(data.end_date)}</td>
+                  <td>{renderDate(data.reasoning_date)}</td>
+                  <td>{data.cancellation_condition}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
     );
+  };
+
+  const getDetailInfo = () => {
+    const infoFields = [
+      { label: t('dateOfBirth'),
+        value: data.date_of_birth ? data.date_of_birth : data.year_of_birth,
+        render: (v) => renderDate(v),
+      },
+      { label: t('placeOfBirth'), value: data.place_of_birth, render: (v) => upFirstLetter(v) },
+      { label: t('address'), value: data.address, render: (v) => v },
+      { label: t('countriesOfCitizenship'),
+        value: data.countries_of_citizenship,
+        render: (v) => v.map((country) => upFirstLetter(country[`name_${i18n.language}`])).join(', '),
+      },
+      { label: t('fullnameInTheLangOfCountryOfCitizenship'),
+        value: data.full_name_original,
+        render: (v) => v,
+      },
+      { label: t('position'), value: data.occupation, render: (v) => v },
+      { label: t('status'),
+        value: data.pep,
+        render: (v) => (v === true ? t('politicallyExposedPerson') : t('notPoliticallyExposedPerson')),
+      },
+      { label: t('documentsInfo'), value: data.id_card, render: (v) => v },
+      { label: t('taxpayerNumber'), value: data.taxpayer_number, render: (v) => v },
+      { label: t('referenceData'), value: data.additional_info, render: (v) => v },
+    ];
+    return infoFields.map((info, i) => (info.value ? (
+      <div className="pl-5 mb-1 flex" key={i}>
+        <div className="w-4/12 pr-1 font-medium">{info.label}:</div>
+        <div className="w-4/6 self-end ">{info.render(info.value)}</div>
+      </div>
+    ) : null));
   };
 
   useEffect(() => {
     fetchData();
+    dispatch(setTopBarShow(false));
+    return () => {
+      dispatch(setTopBarShow(true));
+    };
   }, []);
 
   if (!Object.keys(data).length) {
@@ -46,105 +106,77 @@ const PersonSanctionDetail = () => {
   }
 
   return (
-    <>
-      <div className="intro-y flex items-center mt-8">
-        <h2 className="text-lg mr-auto">
-          {t('personInfo')}
-        </h2>
-        {/* TODO: Add 'Return to search results' button */}
-        {/*{history.location.state?.fromProjects && (*/}
-        {/*  <Button onClick={() => history.goBack()}
-          className="bg-opacity-0 text-blue-800 h-2 mt-3">*/}
-        {/*    <ArrowLeft className="w-10 h-5" />*/}
-        {/*    <span className="underline">{t('returnToTheProject')}</span>*/}
-        {/*  </Button>*/}
-        {/*)}*/}
+    <div className="mt-5 col-span-12 lg:col-span-6 box">
+      <div className="py-2 pr-5 border-b border-gray-200 text-blue-800 flex flex-row font-medium justify-between">
+        <div className="inline-flex">
+          <a onClick={() => history.goBack()} className="inline-flex pt-2 bg-opacity-0 text-blue-800 font-bold cursor-pointer">
+            <ArrowLeft className="w-10 h-5" />
+            {t('back')}
+          </a>
+        </div>
+        <Tooltip
+          position="bottom"
+          arrow={false}
+          content={t('inDevelopment')}
+          className="cursor-default"
+        >
+          <div className="inline-flex mr-8 pt-2">
+            <Printer className="w-5 h-5 mr-1" />
+            {t('print')}
+          </div>
+          <div className="inline-flex">
+            <Download className="w-5 h-5 mr-1" />
+            {t('export.downloadPdf')}
+          </div>
+        </Tooltip>
       </div>
-      <div className="col-span-12 lg:col-span-6">
-        <div className="intro-y space-y-1 mt-8 box">
-          <div className="py-4 pl-5 border-b border-gray-200 flex flex-row">
-            <h2 className="text-2xl font-medium mr-auto capitalize">
+      <div className="intro-y space-y-1 block lg:flex flex-row justify-around">
+        <div className="py-4 px-5 self-auto content-around">
+          <ImgPerson />
+        </div>
+        <div className="block flex flex-col md:w-3/5">
+          <div className="py-4 pl-5 max-w-screen-sm">
+            <h2 className="text-3xl font-bold mr-auto capitalize">
               {data.full_name}
             </h2>
-            <Tooltip
-              position="bottom"
-              arrow={false}
-              content={t('inDevelopment')}
-              className="flex mr-16 cursor-default text-blue-500 pt-3"
-            >
-              <Download className="w-5 h-5 mr-1 color-blue-500" />
-              {t('export.downloadPdf')}
-            </Tooltip>
           </div>
-          <div className="pl-5 flex flex-row">
-            <div className="w-64 font-medium">ID:</div>
-            <div className="max-w-xl">{data.id}</div>
-          </div>
-          <div className="pl-5 flex flex-row">
-            <div className="w-64 font-medium">{t('sanctionStartDate')}:</div>
-            <div className="max-w-xl">{renderDate(data.start_date)}</div>
-          </div>
-          <div className="pl-5 flex flex-row">
-            <div className="w-64 font-medium">{t('sanctionEndDate')}:</div>
-            <div className="max-w-xl">{renderDate(data.end_date)}</div>
-          </div>
-          <div className="pl-5 flex flex-row">
-            <div className="w-64 font-medium">{t('publicExpoused')}:</div>
-            <div className="max-w-xl">{data.pep ? t('politicallyExposedPerson') : t('notPoliticallyExposedPerson')}</div>
-          </div>
-          <div className="pl-5 flex flex-row">
-            <div className="w-64 font-medium">{t('dateOfBirth')}:</div>
-            <div className="max-w-xl">
-              {data.date_of_birth ? (
-                renderDate(data.date_of_birth)
-              ) : data.year_of_birth || '---'}
-            </div>
-          </div>
-          <div className="pl-5 flex flex-row">
-            <div className="w-64 font-medium">{t('placeOfBirth')}:</div>
-            <div className="max-w-xl">{data.place_of_birth || '---'}</div>
-          </div>
-          <div className="pl-5 flex flex-row">
-            <div className="w-64 font-medium">{t('address')}:</div>
-            <div className="max-w-xl">{data.address || '---'}</div>
-          </div>
-          <div className="pl-5 flex flex-row">
-            <div className="w-64 font-medium">{t('countriesOfCitizenship')}:</div>
-            <div className="max-w-xl capitalize">
-              {data.countries_of_citizenship.map((country) => country[`name_${i18n.language}`]).join(', ') || '---'}
-            </div>
-          </div>
-          <div className="pl-5 flex flex-row">
-            <div className="w-64 font-medium">{t('position')}:</div>
-            <div className="max-w-xl">{data.occupation || '---'}</div>
-          </div>
-          <div className="pl-5 flex flex-row">
-            <div className="w-64 font-medium">{t('documentsInfo')}:</div>
-            <div className="max-w-xl">{data.id_card || '---'}</div>
-          </div>
-          <div className="pl-5 flex flex-row">
-            <div className="w-64 font-medium">{t('taxpayerNumber')}:</div>
-            <div className="max-w-xl">{data.taxpayer_number || '---'}</div>
-          </div>
-          <div className="pl-5 flex flex-row">
-            <div className="w-64 font-medium">{t('referenceData')}:</div>
-            <div className="max-w-xl">{data.additional_info || '---'}</div>
-          </div>
-          <div className="pl-5 flex flex-row">
-            <div className="w-64 font-medium">{t('reasoning')}:</div>
-            <div className="max-w-xl">{data.reasoning || '---'}</div>
-          </div>
-          <div className="pl-5 flex flex-row">
-            <div className="w-64 font-medium">{t('sanctionsDetail')}:</div>
-            <div className="max-w-xl">
-              <UnfoldingBlock>
-                {getSanctions(data.types_of_sanctions)}
-              </UnfoldingBlock>
-            </div>
+          <div>
+            {getDetailInfo()}
           </div>
         </div>
+        <div className="py-4 text-gray-500 text-right mr-4">
+          <div>ID: {data.id}</div>
+          <div>{`${t('updatedAt')}: `}{renderDate(data.updated_at)}</div>
+        </div>
       </div>
-    </>
+      <div>
+        {data.decree && (
+          <div className="intro-y pl-5 mt-8 font-bold text-lg">
+            {`${t('sanctionsUnderThePresidentialDecree', { numberDecree: data.decree })} `}{renderDate(data.reasoning_date)}
+          </div>
+        )}
+        <div className="px-5 flex flex-row">
+          {getSanctions(data.types_of_sanctions)}
+        </div>
+      </div>
+      <div className="pt-5 pb-10 pr-5 text-blue-800 flex flex-row justify-end font-medium">
+        <Tooltip
+          position="bottom"
+          arrow={false}
+          content={t('inDevelopment')}
+          className="cursor-default"
+        >
+          <div className="inline-flex mr-8">
+            <Printer className="w-5 h-5 mr-1" />
+            {t('print')}
+          </div>
+          <div className="inline-flex">
+            <Download className="w-5 h-5 mr-1" />
+            {t('export.downloadPdf')}
+          </div>
+        </Tooltip>
+      </div>
+    </div>
   );
 };
 
