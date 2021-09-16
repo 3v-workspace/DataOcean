6fc98060df215bb2af81709c6e8d19ad39747da4
Andrii17s <75915283+Andrii17s@users.noreply.github.com>
2021-09-16 13:53:59 +0300

    its work 2

--- src/components/filter-fields/DatePicker2.jsx
+++ src/components/filter-fields/DatePicker2.jsx
@@ -2,36 +2,46 @@ import React, { useState, useEffect, Component } from 'react';
 import PropTypes from 'prop-types';
 import { ChevronDown, ChevronLeft, ChevronRight, ChevronUp } from 'react-feather';
 import { useTranslation } from 'react-i18next';
-import { isEqualArray } from 'utils';
 import 'styles/datepiker2.scss';
 import { useFormik } from 'formik';
-import Yup from '../../utils/yup';
-import Api, { passErrorsToFormik } from '../../api';
+import Yup from 'utils/yup';
+import moment from 'moment';
+import { Form } from '../form-components';
+
 
 const DatePicker2 = (props) => {
   const { name, options, onChange, value } = props;
   const { t } = useTranslation();
 
-  const [dayValue, setDayValue] = useState();
-  const [monthValue, setMonthValue] = useState();
-  const [yearValue, setYearValue] = useState();
   const [isShowDropdown, setShowDropdown] = useState(false);
   const currentDate = new Date();
   const [currentYear, setCurrentYear] = useState(currentDate.getFullYear());
 
   const formik = useFormik({
     initialValues: {
-      inputDay: '',
-      inputMonth: '',
-      inputYear: '',
+      day: '',
+      month: '',
+      year: '',
     },
     validationSchema: Yup.object({
-      inputDay: Yup.number(),
-      inputMonth: Yup.number(),
-      inputYear: Yup.number().required().test('value', 'Must be in YYYY format', (val) => val > 1919 && val < 2032),
+      day: Yup.number()
+        .min(1, 'Day must be more than 1!')
+        .max(31, 'Day must be less than 31!'),
+      month: Yup.string(),
+      year: Yup.number()
+        .required()
+        .min(1920, 'Year must be after 1919!')
+        .max(2039, 'Year must be before 2040!'),
     }),
-    onSubmit: (values, actions) => {
-      console.log(5454);
+    onSubmit: (values) => {
+      let data = '';
+      data += ((typeof formik.values.day === 'string') ? formik.values.day : formik.values.day[0]);
+      data += ' ';
+      data += ((typeof formik.values.month === 'string') ? formik.values.month : formik.values.month[0]);
+      data += ' ';
+      data += ((typeof formik.values.year === 'string') ? formik.values.year : formik.values.year[0]);
+      console.log(data);
+      console.log(moment(data, ['uk', 'en']).isValid());
     },
   });
 
@@ -48,9 +58,9 @@ const DatePicker2 = (props) => {
   };
 
   function onClear(number) {
-    setDayValue('');
-    setMonthValue('');
-    setYearValue('');
+    formik.setFieldValue('day', '');
+    formik.setFieldValue('month', '');
+    formik.setFieldValue('year', '');
   }
 
   function createDay() {
@@ -89,7 +99,7 @@ const DatePicker2 = (props) => {
           readOnly
           type="text"
           className="input text-gray-600 w-40"
-          value={t('selected', { optionsCount: options.length })}
+          value=""
           onClick={showDropdown}
         />
         {isShowDropdown ? (
@@ -102,81 +112,92 @@ const DatePicker2 = (props) => {
         className={`w-1/2 sm:w-auto mt-1 absolute max-w-3xl select-dropdown ${isShowDropdown ? 'show' : ''}`}
         onMouseLeave={hideDropdown}
       >
-        <div className="select-dropdown__content">
-          <div className="input-container">
-            <div className="input-day">
-              <input
-                className="inputDay"
-                type="text"
-                size="2"
-                placeholder="XX"
-                id="day"
-                name="day"
-                value={dayValue}
-                formik={formik}
-              />
-            </div>
-            <div className="dropdown">
-              <button type="button" className="dropbtn">
-                <ChevronDown className="w-4 h-6" />
-              </button>
-              <div className="dropdown-content">
-                {createDay().map((day) => (<button type="button" className="dayButton" onClick={() => setDayValue(day)}>{day}</button>))}
+        <Form formik={formik} onSubmit={formik.handleSubmit}>
+          <div className="select-dropdown__content">
+            <div className="input-container">
+              <div className="input-day">
+                <input
+                  className="inputDay"
+                  type="text"
+                  size="2"
+                  placeholder="XX"
+                  id="day"
+                  name="day"
+                  onChange={formik.handleChange}
+                  value={formik.values.day}
+                  formik={formik}
+                />
+                {formik.errors.day && formik.touched.day && (
+                  <p className="errorMessage">{formik.errors.day}</p>
+                )}
               </div>
-            </div>
-            <div className="input-month">
-              <input
-                className="inputMonth"
-                type="text"
-                size="15"
-                placeholder={t('month')}
-                value={monthValue}
-                id="month"
-                name="month"
-                formik={formik}
-              />
-            </div>
-            <div className="dropdown">
-              <button type="button" className="dropbtn">
-                <ChevronDown className="w-4 h-6" />
-              </button>
-              <div className="dropdown-content">
-                {createMonth().map((month) => (<button type="button" className="monthButton" onClick={() => setMonthValue(t(month))}>{t(month)}</button>))}
-              </div>
-            </div>
-            <div className="input-year">
-              <input
-                className="inputYear"
-                required
-                type="text"
-                min="1921"
-                max="2041"
-                size="4"
-                placeholder="XXXX"
-                id="year"
-                name="year"
-                value={yearValue}
-                formik={formik}
-              />
-            </div>
-            <div className="dropdown">
-              <button type="button" className="dropbtn">
-                <ChevronDown className="w-4 h-6" />
-              </button>
-              <div className="dropdown-content">
-                <ChevronLeft onClick={prevPage} className="w-6 h-6 mt+5" style={{ width: '10%', height: '100%', border: 'black solid' }} />
-                <div className="yearsTable">
-                  {createYear().map((year) => (<button type="button" className="yearButton" onClick={() => setYearValue(year)}>{year}</button>))}
+              <div className="dropdown">
+                <button type="button" className="dropbtn">
+                  <ChevronDown className="w-4 h-6" />
+                </button>
+                <div className="dropdown-content">
+                  {createDay().map((day) => (<button type="button" className="dayButton" onClick={() => formik.setFieldValue('day', day)}>{day}</button>))}
                 </div>
-                <ChevronRight onClick={nextPage} className="w-6 h-6 mt+5" style={{ width: '10%', height: '100%', border: 'black solid' }} />
+              </div>
+              <div className="input-month">
+                <input
+                  className="inputMonth"
+                  type="text"
+                  size="15"
+                  placeholder={t('month')}
+                  id="month"
+                  onChange={formik.handleChange}
+                  value={formik.values.month}
+                  formik={formik}
+                />
+                {formik.errors.month && formik.touched.month && (
+                  <p className="errorMessage">{formik.errors.month}</p>
+                )}
+              </div>
+              <div className="dropdown">
+                <button type="button" className="dropbtn">
+                  <ChevronDown className="w-4 h-6" />
+                </button>
+                <div className="dropdown-content">
+                  {createMonth().map((month) => (<button type="button" className="monthButton" onClick={() => formik.setFieldValue('month', t(month))}>{t(month)}</button>))}
+                </div>
+              </div>
+              <div className="input-year">
+                <input
+                  className="inputYear"
+                  required
+                  type="text"
+                  size="4"
+                  placeholder="XXXX"
+                  id="year"
+                  name="year"
+                  onChange={formik.handleChange}
+                  value={formik.values.year}
+                  formik={formik}
+                />
+                {formik.errors.year && formik.touched.year && (
+                  <p className="errorMessage">{formik.errors.year}</p>
+                )}
+              </div>
+              <div className="dropdown">
+                <button type="button" className="dropbtn">
+                  <ChevronDown className="w-4 h-6" />
+                </button>
+                <div className="dropdown-content" style={{ 'align-items': 'center' }}>
+                  <div className="chevron"><ChevronLeft onClick={prevPage} className="w-6 h-6 mt+5" /></div>
+                  <div className="yearsTable">
+                    {createYear().map((year) => (<button type="button" className="yearButton" onClick={() => formik.setFieldValue('year', year)}>{year}</button>))}
+                  </div>
+                  <div className="chevron"><ChevronRight onClick={nextPage} className="w-6 h-6 mt+5" /></div>
+                </div>
               </div>
             </div>
+            <div className="button-container">
+              <button type="button" onClick={onClear} className="cancelButton">{t('cancel')}</button>
+              <button type="submit" className="okButton">OK</button>
+            </div>
           </div>
-          <div className="button-container">
-            <button type="button" onClick={onClear} className="cancelButton">{t('cancel')}</button>
-            <button type="submit" className="okButton">OK</button>
-          </div>
-        </div>
+        </Form>
       </div>
     </>
   );
