0db52b3437625d7be90358a8c92c59c38d823391
Roman Tiukh <frogour@gmail.com>
2021-12-16 16:25:00 +0200

    move testing vars to .env

--- src/const/testing.js
+++ src/const/testing.js
@@ -1,6 +1,6 @@
-// FIXME: temporary variables for hiding functional
-export const HIDE_EXPORT_BUTTON = true;
-export const HIDE_FILTERS = false;
-export const HIDE_SELECT_COLUMNS = false;
-export const PERSON_DEBUG = false;
-export const COMPANY_SEARCH_TESTING = false;
+// temporary variables for hiding functional
+export const HIDE_EXPORT_BUTTON = process.env.REACT_APP_HIDE_EXPORT_BUTTON === 'true';
+export const HIDE_FILTERS = process.env.REACT_APP_HIDE_FILTERS === 'true';
+export const HIDE_SELECT_COLUMNS = process.env.REACT_APP_HIDE_SELECT_COLUMNS === 'true';
+export const PERSON_DEBUG = process.env.REACT_APP_PERSON_DEBUG === 'true';
+export const COMPANY_SEARCH_TESTING = process.env.REACT_APP_COMPANY_SEARCH_TESTING === 'true';
