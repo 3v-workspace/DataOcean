d38613d8cfa1c3c70c84074004061c88c7aa8e1e
IegorSoboliev <evsobolev@gmail.com>
2021-12-27 17:43:21 +0200

    Changed SanctionBlock

--- src/components/blocks/SanctionBlock.jsx
+++ src/components/blocks/SanctionBlock.jsx
@@ -19,6 +19,9 @@ const SanctionBlock = (props) => {
 
   return (
     <>
+      <tr className="py-3 float-left">
+        {t('sanctionsCounter')} {`${sanctions.activeSanction.length + sanctions.inactiveSanction.length}` }
+      </tr>
       <table className="table text-center">
         <thead>
           <tr className="bg-gray-200 text-gray-700">
@@ -30,13 +33,10 @@ const SanctionBlock = (props) => {
             <th>{t('cancelingConditions')}</th>
           </tr>
         </thead>
-        <tbody className="rounded-sm border-none" style={{ background: '#FFF4F4', boxShadow: 'inset 0 0 0 1px #B10000', outline: '2px solid rgb(177 0 0)', outlineOffset: '-3px' }}>
-          <tr className="h-6">
-            <td className="p-0">
-              <span className="px-5 float-left font-bold text-white" style={{ background: '#B10000', borderRadius: '2px 0px' }}>{t('activeSanctions')}</span>
-            </td>
-            <td className="p-0" colSpan="5" />
-          </tr>
+        <tr className="px-5 py-3 float-left font-bold">
+          {t('activeSanctions')}
+        </tr>
+        <tbody className="rounded-sm border-none" style={{ background: '#FFF4F4' }}>
           {sanctions.activeSanction.map((sanction) => (
             sanction.name_of_sanction.map((type, i) => (
               <tr key={i} className={i === 0 ? '' : 'border-t border-gray-400'}>
@@ -50,10 +50,13 @@ const SanctionBlock = (props) => {
             ))
           ))}
         </tbody>
+        <tr className="py-3 float-left">
+          {t('expiredSanctions')}
+        </tr>
         <tbody>
           {sanctions.inactiveSanction.map((sanction) => (
             sanction.name_of_sanction.map((type, i) => (
-              <tr key={i} className="border-l border-r border-l border-b border-gray-400">
+              <tr key={i} className="border-t border-r border-b border-gray-400">
                 <td className="mt-0.5 text-left">{type}</td>
                 <td>{`${t('numberDecreeFrom', { numberDecree: sanction.decree })} `}{renderDate(sanction.start_date)}</td>
                 <td>{renderDate(sanction.start_date)}</td>
