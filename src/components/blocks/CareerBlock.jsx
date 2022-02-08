7bf4f36f01c9b3ed3a7fad1fabfcfb7b73dd9b9a
Roman Tiukh <frogour@gmail.com>
2022-02-08 09:51:37 +0200

    fix position translations

--- src/components/blocks/CareerBlock.jsx
+++ src/components/blocks/CareerBlock.jsx
@@ -2,18 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { DataSourceLabel } from 'components/pages/datasets/person/DataSourceLabel';
 import { getLocaleField } from 'utils';
+import { useTranslation } from 'react-i18next';
 
 const CareerBlock = (props) => {
   const { data, person } = props;
+  const { t } = useTranslation();
+
   return (
     <ul className="list-none">
       {data.map((pos, i) => (
         <li key={i} className="grid grid-cols-10">
           <span className="col-span-2">
-            {pos.year ? pos.year : 'Остання відома'}
+            {pos.year ? pos.year : t('lastKnown')}
           </span>
           <span className="col-span-4">
-            {getLocaleField(pos.position)}
+            {getLocaleField(pos, 'position')}
           </span>
           <span className="col-span-4">
             <DataSourceLabel person={person} data={pos} noBrackets />
